b1013e096e6ad3c088fe9393dffa981556804e16
Anna Stefano Narivelomanana <annanarivelomanana07@gmail.com>
2023-05-27 20:37:19 +0100

    Improvement

--- Berlekamp_Massey.py
+++ Berlekamp_Massey.py
@@ -1,64 +1,53 @@
-#!/usr/bin/env python
-# coding: utf-8
+import sympy as sy              # importation of sympy
+sy.init_printing()              # output latex display
+x = sy.Symbol('x')
 
-# In[1]:
 
-
-import sympy as sy             #importation de sympy
-sy.init_printing()             #affichage en latex des outputs
-x=sy.Symbol('x')
 def Berlekamp(seq):
-    f=1
-    deg_f_list=[]
-    n=len(seq)
-    f_list=[1]
+    f = 1
+    deg_f_list = []
+    n = len(seq)
+    f_list = [1]
     for i in range(n):
-        #print("Step",i+1)
-        d=0
-        deg_f=sy.degree(f)
+        d = 0
+        print("step i = ", i)
+        deg_f = sy.degree(f)
         deg_f_list.append(deg_f)
-        coeffs_f=sy.Poly(f,x).all_coeffs()
+        coeffs_f = sy.Poly(f, x).all_coeffs()   
+        if coeffs_f != []:
+            coeffs_f = coeffs_f[-1::-1]
         for j in range(deg_f+1):
-            d=d+coeffs_f[deg_f-j] * seq[i-j]
-        if d==0:
-            f=sy.Poly(f,x).trunc(2)
+            d = d+coeffs_f[deg_f-j] * seq[i-j]
+            d = d % 2
+        if d == 0:
+            f = sy.Poly(f, x).trunc(2)
             f_list.append(f)
         else:
-            L_index=[k for k in range(len(deg_f_list)-1) if deg_f_list[k]<deg_f_list[k+1]]
-            if len(L_index)==0:
-                m=-1
-                fm=1
-                Lm=0
+            L_index = [k for k in range(
+                len(deg_f_list)-1) if deg_f_list[k] < deg_f_list[k+1]]
+            if len(L_index) == 0:
+                m = -1
+                Lm = 0
+                fm = 1
             else:
-                m=max(L_index)
-                fm=f_list[m]
-                Lm=deg_f_list[m]
-            #print("deg of f = ", deg_f)
-            #print("m = ",m)
-            #print("list of f = ",f_list)
-                Li=deg_f_list[i]
-                if (m-Lm)>=(i-Li):
-                    f=f+x**((m-Lm)-(i-Li))*fm
-                    f=sy.Poly(f,x).trunc(2)
-                    f_list.append(f)
-                else:
-                    f=(x**((i-Li)-(m-Lm)))*(f)+fm
-                    f=sy.Poly(f,x).trunc(2)
-                    f_list.append(f)
-        #print(deg_f_list)    
-        #print(f)
-    return f.trunc(2)
+                m = max(L_index)
+                Lm = deg_f_list[m]
+                fm = f_list[m]
+            Li = deg_f_list[i]
+            if (m-Lm) >= (i-Li):
+                f = f+x**((m-Lm)-(i-Li))*fm
+                f = sy.Poly(f, x).trunc(2)
+                f_list.append(f)
+            else:
+                f = (x**((i-Li)-(m-Lm)))*(f)+fm
+                f = sy.Poly(f, x).trunc(2)
+                f_list.append(f)
+        print('f({})='.format(i+1), f)
+
+    return f
 
 
-# In[2]:
-
-
-seq=[1, 1, 0, 1, 0, 0, 1]
-Berlekamp(seq)
-
-
-# In[ ]:
-
-
-
-
+# driver code
+if __name__ == "__main__":
+    seq = [1, 1, 0, 1, 0, 0, 1]
+    Berlekamp(seq)
